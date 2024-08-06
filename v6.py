1faefaeaa19f7e6a1efd1b1633607fcc9f338f5e
vinnu-lgs <vsnlgs123@gmail.com>
2024-08-06 00:12:00 -0700

    Create v6.py

---
+++ v6.py
@@ -0,0 +1,9 @@
+ram=int(input("Enter a age of Ram:"))
+shyam=int(input("Enter a age of Shyam:"))
+ajay=int(input("Enter a age of Shyam:"))
+if (ram>shyam)and ( ram>ajay):
+print("Ram is youngest.")
+elif shyam>ram and shyam>ajay:
+print("Shyam is youngest.")
+else:
+print("Ajay is youngest.")
